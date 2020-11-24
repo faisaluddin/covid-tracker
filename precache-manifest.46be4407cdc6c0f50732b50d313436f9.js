4e5375c272d385bae4f7483217fff512d5ca8528
faisal <faisaluddin01@gmail.com>
2020-11-24 15:44:36 +0530

    Updates

---
+++ precache-manifest.46be4407cdc6c0f50732b50d313436f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca5d07146f851a92f186f79d3c7bb43",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "c381a801b7a5c0edd196",
+    "url": "/covid-tracker/static/css/main.c4254182.chunk.css"
+  },
+  {
+    "revision": "c0ed790609bc208f4c1d",
+    "url": "/covid-tracker/static/js/2.b17163d8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.b17163d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c381a801b7a5c0edd196",
+    "url": "/covid-tracker/static/js/main.9f6a57c0.chunk.js"
+  },
+  {
+    "revision": "ef7fedef9cd7db49b0f7",
+    "url": "/covid-tracker/static/js/runtime-main.adeb3781.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
